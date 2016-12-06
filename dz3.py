8474cdb2c5a961e88728f1e342594a4eb75225e4
Tamara Zdorova <toma-kukina@yandex.ru>
2016-12-06 22:59:12 +0300

    Revert "upd домашка про множества"
    This reverts commit b1b3d3ce99bd41b2c164e71424d7b19f374f199f.

--- dz3.py
+++ dz3.py
@@ -143,12 +143,7 @@ def raz_again():
     for i, m in cw4.items():
         if m > 1:  
             moon.append(i)
-    dada = Counter(moon)  #если слово в массиве несколько раз, значит оно не только в одной статье, правда?
-    wow = [] 
-    for s, h in dada.items():
-        if h == 1:
-            wow.append(s)
-    sun = set(wow) 
+    sun = set(moon) #чтобы убрать повторяющиеся слова
     life = sun & result_raz #цель задания
     f_raz2 = open('again_raz.txt', 'w', encoding = 'utf-8')
     arr_life = []
